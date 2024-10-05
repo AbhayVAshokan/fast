837c9c6096f6330b34b58fd69424a5657a497849
Abhay V Ashokan <abhayvashokan@gmail.com>
2024-10-06 00:37:38 +0530

    Added nuxt-icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          Orbitron: [400],
-        },
+  modules: ["@nuxtjs/tailwindcss", [
+    "@nuxtjs/google-fonts",
+    {
+      families: {
+        Orbitron: [400],
       },
-    ],
-  ],
+    },
+  ], "@nuxt/icon"],
 });
